b5f0d7dd6ae318af53030644d70ae42d222d6cc5
sumin oh <osm0644@gmail.com>
2023-07-17 09:55:09 +0900

    Create Config.java

---
+++ sp_class/json/Config.java
@@ -0,0 +1,12 @@
+package study;
+ 
+import java.util.List;
+ 
+public class Config {
+    private List<Pipelines> PIPELINES;
+ 
+    @Override
+    public String toString() {
+        return "Config [PIPELINES=" + PIPELINES + "]";
+    }
+}
