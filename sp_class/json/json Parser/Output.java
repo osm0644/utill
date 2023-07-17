fab25c7a2eed27cddd01d12802ad7c6bb3b15e49
sumin oh <osm0644@gmail.com>
2023-07-17 09:58:36 +0900

    Create Output.java

---
+++ sp_class/json/json Parser/Output.java
@@ -0,0 +1,13 @@
+package study;
+ 
+import java.util.List;
+ 
+public class Split {
+    private String DELIMETER;
+    private List<String> RESULT;
+     
+    @Override
+    public String toString() {
+        return "SPLIT [DELIMETER=" + DELIMETER + ", RESULT=" + RESULT + "]";
+    }  
+}
